b1bed113589b73f024842e0753f957ee7211b326
nemimccarter <nemimccarter@gmail.com>
2018-01-22 15:16:59 -0800

    Move P1 into appropriate directory

---
+++ project1/McCarter-RibakoffNP1.py
@@ -0,0 +1,11 @@
+#########################################################################
+# File: McCarter-RibakoffNP1.py                                         #
+# By: Nehemya McCarter-Ribakoff                                         #  
+# Date: [date last revised]                                             #
+# Usage: python McCarter-RibakoffP1.py                                  #
+# System: Linux Debian 9.2                                              #
+# Description: [short description on what program does, what user       #
+# enters, what result(s) are displayed]                                 # 
+#########################################################################
+
+
