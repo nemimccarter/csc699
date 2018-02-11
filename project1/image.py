d2af68d44b92c83cd26f8e12e3383acea5d84fe0
nemimccarter <nemimccarter@gmail.com>
2018-02-11 02:54:45 -0800

    Center image, add border, scale image by args)

--- project1/image.py
+++ project1/image.py
@@ -17,24 +17,30 @@ class Window(QWidget):
             break
         print(self.data_files)
 
-        self.initUI()
+        self.initUI(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
 
-    def initUI(self):
+    def initUI(self, W, H, B):
         self.setWindowTitle(self.title)
-        self.setGeometry(100, 100, 250, 250)
+        self.setGeometry(100, 100, W, H)
 
         hbox = QHBoxLayout(self)
         pixmap = QPixmap("./data/" + self.data_files[0])
 
         label = QLabel(self)
         label.setText("This is a QLabel")
-        label.setStyleSheet("border: 10px solid grey")
-        label.resize(200, 200)
+        label.setStyleSheet("border: " + B + " solid black")
+        label.resize(W, H)
         label.setPixmap(pixmap)
-
         hbox.addWidget(label)
         self.setLayout(hbox)
 
+        if (W > H):
+            pixmap_scaled = pixmap.scaledToWidth(W - (2 * int(B)))
+        else:
+            pixmap_scaled = pixmap.scaledToHeight(H - (2 * int(B)))
+
+        label.setPixmap(pixmap_scaled)
+        label.setAlignment(Qt.AlignCenter)
         self.show()
 
 if __name__ == '__main__':
