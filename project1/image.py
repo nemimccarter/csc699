f31b9fb4a118adb41a1a588f27a44e74af5cf612
nemimccarter <nemimccarter@gmail.com>
2018-02-13 14:36:47 -0800

    correct aspect ratio

--- project1/image.py
+++ project1/image.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QWidget, QLabel, QLCDNumber, QSlider, QHBoxLayout, QApplication)
+from PyQt5.QtWidgets import (QWidget, QLabel, QHBoxLayout, QApplication)
 from PyQt5.QtGui import QPixmap
 
 from os import listdir, walk
@@ -17,33 +17,29 @@ class Window(QWidget):
             break
         self.data_files.sort()
         print(self.data_files)
-
         self.initUI(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
+
+
 
     def initUI(self, W, H, B):
         self.setWindowTitle(self.title)
         self.setGeometry(100, 100, W, H)
-
+        
         hbox = QHBoxLayout(self)
-        pixmap = QPixmap("./data/" + self.data_files[0])
 
         label = QLabel(self)
         label.setText("This is a QLabel")
         label.setStyleSheet("border: " + B + " solid black")
         label.resize(W, H)
+        pixmap = QPixmap('./data/' + self.data_files[0])
+        pixmap = pixmap.scaled(int(W) - (2 * int(B)), int(H) - (2 * int(B)), Qt.KeepAspectRatio)
         label.setPixmap(pixmap)
         hbox.addWidget(label)
         self.setLayout(hbox)
-
-        if (W > H):
-            pixmap_scaled = pixmap.scaledToWidth(W - (2 * int(B)))
-        else:
-            pixmap_scaled = pixmap.scaledToHeight(H - (2 * int(B)))
-
-        label.setPixmap(pixmap_scaled)
+        label.setPixmap(pixmap)
         label.setAlignment(Qt.AlignCenter)
         self.show()
-
+   
 if __name__ == '__main__':
         app = QApplication(sys.argv)
         window = Window()
